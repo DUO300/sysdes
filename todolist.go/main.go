d5e7bdb30fcc25c8f7c0612964f208e5852c90d2
DUO300 <rheilegon@gmail.com>
2022-11-17 12:25:24 +0900

    revised comment

--- todolist.go/main.go
+++ todolist.go/main.go
@@ -18,7 +18,7 @@ import (
 const port = 8000
 
 func main() {
-	// initialize DB connection
+	// Initialize DB connection
 	dsn := db.DefaultDSN(
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
@@ -29,15 +29,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// initialize Gin engine
+	// Initialize Gin engine
 	engine := gin.Default()
 	engine.LoadHTMLGlob("views/*.html")
 
-	// prepare session
+	// Prepare session
 	store := cookie.NewStore([]byte("my-secret"))
 	engine.Use(sessions.Sessions("user-session", store))
 
-	// routing
+	// Routing
 	engine.Static("/assets", "./assets")
 	engine.GET("/", service.Home)
 	engine.GET("/list", service.LoginCheck, service.TaskList)
@@ -45,33 +45,42 @@ func main() {
 	taskGroup := engine.Group("/task")
 	taskGroup.Use(service.LoginCheck)
 	{
+		// Show task info
 		taskGroup.GET("/:id", service.TaskCheck, service.ShowTask) // ":id" is a parameter
 
-		// タスクの新規登録
+		// Register new task
 		taskGroup.GET("/new", service.NewTaskForm)
 		taskGroup.POST("/new", service.RegisterTask)
 
-		// 既存タスクの編集
+		// Edit task
 		taskGroup.GET("/edit/:id", service.TaskCheck, service.EditTaskForm)
 		taskGroup.POST("/edit/:id", service.TaskCheck, service.UpdateTask)
 
-		// 既存タスクの削除
+		// Delete task
 		taskGroup.GET("/delete/:id", service.TaskCheck, service.DeleteTask)
 	}
 
-	// ユーザ登録
+	// Register new user
 	engine.GET("/user/new", service.NewUserForm)
 	engine.POST("/user/new", service.RegisterUser)
 
-	// ユーザログイン
+	// User login
 	engine.GET("/login", service.LoginForm)
 	engine.POST("/login", service.Login)
+
+	// User logout
 	engine.GET("/logout", service.LoginCheck, service.Logout)
+
+	// Delete user account
 	engine.GET("/delete_user", service.LoginCheck, service.DeleteUser)
+
+	// Show user info
 	engine.GET("/user/info", service.LoginCheck, service.ShowUser)
+
+	// Edit user account
 	engine.GET("/user/edit", service.LoginCheck, service.EditUser)
 	engine.POST("/user/edit", service.LoginCheck, service.UpdateUser)
 
-	// start server
+	// Start server
 	engine.Run(fmt.Sprintf(":%d", port))
 }
